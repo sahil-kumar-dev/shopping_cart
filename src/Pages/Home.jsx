fe2cf8a13a42f23d046297f73940925b62a84e68
Sahil Kumar <sahilkumarofficial05@gmail.com>
2023-10-27 01:05:18 +0530

    created files

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+
+	const API_URL = "https://fakestore.api/products"
+  return (
+	<div>Home</div>
+  )
+}
+
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -8,5 +8,5 @@ import { store } from './redux/Store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-  </Provider>,
+  </Provider>
 )
